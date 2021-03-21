013a581d2b8f3cfaab5073cb1a4b5c4ee481c486
MannyHubGeek <ohueus@yahoo.com>
2021-03-21 18:09:51 +0000

    Not working properly needs reworking
    add some random code for practice

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from turtle import Turtle, Screen
-import random
-
-#manny = Turtle()
-screen = Screen()
+# from turtle import Turtle, Screen
+# import random
+#
+# #manny = Turtle()
+# screen = Screen()
 
 #higher order function - passing a fucntion as an input for another function
 
@@ -44,47 +44,82 @@ screen = Screen()
 
 #creating multiple states of an object
 
-race = False
-screen.setup(width=1000, height=600)
-#manny = Turtle("turtle")
-
-colors = ("red", "blue", "green", "orange", "yellow", "purple")
-
-user_guess =screen.textinput(title="Welcome to the Turtle GrandPrix!!!", prompt="Who do you think will win the race?")
-speed = [1, 2, 3, 4, 5, 6]
-pos = [-250, -200, -150, -100, -50, 0]
-my_turtles = []
-for obj in range(0, 6):
-    manny = Turtle(shape="turtle")
-    manny.turtlesize(1.5)
-    manny.penup()
-    manny.goto(x=-485, y=pos[obj])
-    manny.color(colors[obj])
-    my_turtles.append(manny)
-
-
-if user_guess:
-    race = True
-
-while user_guess:
-    for turtle in my_turtles:
-        if turtle.xcor() > 420:
-            race = False
-            winning_color = turtle.pencolor()
-            if winning_color == user_guess:
-                print(f"Your turtle with {winning_color} won the race")
-            else:
-                print("you lost")
-        rand_distance = random.randint(0, 15)
-        turtle.forward(rand_distance)
-
-
+# race = False
+# screen.setup(width=1000, height=600)
+# #manny = Turtle("turtle")
+#
+# colors = ("red", "blue", "green", "orange", "yellow", "purple")
+#
+# user_guess =screen.textinput(title="Welcome to the Turtle GrandPrix!!!", prompt="Who do you think will win the race?")
+# speed = [1, 2, 3, 4, 5, 6]
+# pos = [-250, -200, -150, -100, -50, 0]
+# my_turtles = []
+# for obj in range(0, 6):
+#     manny = Turtle(shape="turtle")
+#     manny.turtlesize(1.5)
+#     manny.penup()
+#     manny.goto(x=-485, y=pos[obj])
+#     manny.color(colors[obj])
+#     my_turtles.append(manny)
+#
+#
+# if user_guess:
+#     race = True
+#
+# while user_guess:
+#     for turtle in my_turtles:
+#         if turtle.xcor() > 420:
+#             race = False
+#             winning_color = turtle.pencolor()
+#             if winning_color == user_guess:
+#                 print(f"Your turtle with {winning_color} won the race")
+#             else:
+#                 print("you lost")
+#         rand_distance = random.randint(0, 15)
+#         turtle.forward(rand_distance)
 
 
+def greeting(name):
+    out = name.title()
+    return f'Hi {out}'
+
+
+# print(greeting("emmanuel"))
+#
+# phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+#
+# for key, value in phone_numbers.items():
+#
+#     print("%s: %s" % (key, value))
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+
+for value in phone_numbers.values():
+    value.replace("+", "00")
+
+    print(f"{value}")
+
+
+#screen.exitonclick()
+
+
+
+a = 0
+while a < 5:
+    a = a + 1
+    print(a)
 
 
 
 
 
 
-screen.exitonclick()
+
+
+
+
+
+
+
+
+
